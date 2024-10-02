03e422177a90c8e734b41492cf6204ebbad36bf9
Arslan Giniiatullin <arslan-gin@yandex-team.ru>
2024-10-02 20:26:18 +0300

    Added gateway api for core-booking

--- gateway/src/main/kotlin/config/ApplicationConfiguration.kt
+++ gateway/src/main/kotlin/config/ApplicationConfiguration.kt
@@ -1,9 +1,14 @@
 package org.example.config
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
 
 @Configuration
 @EnableConfigurationProperties(UserAuthorities::class)
 class ApplicationConfiguration {
+
+    @Bean
+    fun restTemplate() = RestTemplate()
 }
