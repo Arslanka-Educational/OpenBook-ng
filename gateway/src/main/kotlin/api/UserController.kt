2333c52ab107ff91145715de9c8e7b0cc90e8133
Arslan Giniiatullin <arslan-gin@yandex-team.ru>
2024-10-02 20:14:59 +0300

    Added booking api in gateway

--- gateway/src/main/kotlin/api/UserController.kt
+++ gateway/src/main/kotlin/api/UserController.kt
@@ -4,22 +4,18 @@ import model.AuthorizationUserDetails
 import org.example.service.security.AuthorizationUserDetailsService
 import org.example.service.security.JwtService
 import org.springframework.http.ResponseEntity
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import ru.openbook.api.UsersApi
-import ru.openbook.model.UserInfoResponse
 import ru.openbook.model.UserRegisterRequest
 import ru.openbook.model.UserRegisterResponse
-import java.util.*
 
 @RestController
 class UserController(
     private val userService: AuthorizationUserDetailsService,
     private val jwtService: JwtService,
-    private val authenticationManager: AuthenticationManager,
 ) : UsersApi {
     @RequestMapping(
         method = [RequestMethod.POST],
@@ -36,10 +32,5 @@ class UserController(
         userRegisterRequest: UserRegisterRequest
     ): ResponseEntity<UserRegisterResponse> {
          return ResponseEntity.ok().body(UserRegisterResponse(userService.registerUser(userRegisterRequest).id.toString()))
-    }
-
-    override fun userInfo(userId: UUID): ResponseEntity<UserInfoResponse> {
-//        return ResponseEntity.ok(UserInfoResponse("name", "email"))
-        TODO("Not yet implemented")
     }
 }
