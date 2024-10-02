45466522422b31f724b10b0c4325604c4b3c52b2
“Arslan <ars2003lan@gmail.com>
2024-10-02 09:12:09 +0000

    Fixed type

--- core-booking/src/main/kotlin/stream/kafka/CoreCatalogEventsListener.kt
+++ core-booking/src/main/kotlin/stream/kafka/CoreCatalogEventsListener.kt
@@ -1,9 +1,9 @@
 package org.example.stream.kafka
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.example.model.Reservation
 import org.example.model.ReservationRepository
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 import java.time.Instant
@@ -16,36 +16,36 @@ class CoreCatalogEventsListener(
 ) {
 
     @KafkaListener(topics = ["\${spring.kafka.consumer.topic.name}"], groupId = "\${spring.kafka.consumer.group-id}")
-    internal fun consume(message: String) {
+    internal fun resolvedReservationStatus(message: String) {
         val reservationEvent: ReservationEvent = objectMapper.readValue(message, ReservationEvent::class.java)
 
         val updatedStatus = when (reservationEvent.status) {
             "SUCCESS" -> Reservation.ReservationStatus.SUCCESS
             "FAILED" -> Reservation.ReservationStatus.FAILED
-            else -> return // Unrecognized status, perhaps log it or handle it
+            else -> Reservation.ReservationStatus.IN_PROGRESS
         }
 
         reservationRepository.updateWithIdAndStatus(
-            Reservation(
-                reservationEvent.id,
-                reservationEvent.userId,
-                reservationEvent.bookId,
-                reservationEvent.createdAt,
-                reservationEvent.updatedAt,
-                updatedStatus,
-                reservationEvent.reason
-            ),
-            Reservation.ReservationStatus.IN_PROGRESS
+            id = reservationEvent.id,
+            uid = reservationEvent.userId,
+            bookId = reservationEvent.bookId,
+            updatedAt = Instant.now(),
+            status = updatedStatus,
+            reason = reservationEvent.reason,
+            expectedStatus = Reservation.ReservationStatus.IN_PROGRESS,
         )!!
     }
 
     data class ReservationEvent(
+        @JsonProperty("external_id")
         val id: UUID,
+        @JsonProperty("user_id")
         val userId: UUID,
+        @JsonProperty("book_id")
         val bookId: UUID,
-        val createdAt: Instant,
-        val updatedAt: Instant,
+        @JsonProperty("status")
         val status: String,
+        @JsonProperty("reason")
         val reason: String?,
     )
 }
