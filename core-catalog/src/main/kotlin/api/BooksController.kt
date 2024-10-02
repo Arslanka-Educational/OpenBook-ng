914bb9d83771fe15531fff4dae8ac7159e72c4ab
kanat <kanttoroev@gmail.com>
2024-10-02 17:14:17 +0300

    kafka configuration

--- core-catalog/src/main/kotlin/api/BooksController.kt
+++ core-catalog/src/main/kotlin/api/BooksController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import ru.openbook.api.BooksApi
 import ru.openbook.model.BookContentResponse
 import ru.openbook.model.BookInstanceResponse
+import ru.openbook.model.BookReservationInitializationRequest
 import java.util.*
 
 class BooksController(
@@ -19,14 +20,17 @@ class BooksController(
         TODO("Not yet implemented")
     }
 
-    override fun getBookInstancesByBookContentId(bookContentId: String): ResponseEntity<List<BookInstanceResponse>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun initializeBookReservation(bookInstanceId: UUID): ResponseEntity<BookInstanceResponse> {
+    override fun initializeBookReservation(
+        bookInstanceId: UUID,
+        bookReservationInitializationRequest: BookReservationInitializationRequest
+    ): ResponseEntity<BookInstanceResponse> {
         val bookInstance: BookInstance? = bookInstanceService.initializeBookInstanceReservation(bookInstanceId)
         return bookInstance?.let {
             ResponseEntity.ok(bookInstance.mapToResponse())
         } ?: ResponseEntity.notFound().build();
+    }
+
+    override fun getBookInstancesByBookContentId(bookContentId: String): ResponseEntity<List<BookInstanceResponse>> {
+        TODO("Not yet implemented")
     }
 }
--- core-catalog/src/main/kotlin/kafka/BookReservationProducer.kt
+++ core-catalog/src/main/kotlin/kafka/BookReservationProducer.kt
@@ -1,12 +1,15 @@
 package org.example.kafka
 
 import org.example.model.BookInstance
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class BookReservationProducer(
-    private val kafkaTemplate: KafkaTemplate<String, BookInstance>
+    private val kafkaTemplate: KafkaTemplate<String, BookInstance>,
+//    @Value("\${spring.kafka.producer.topic.name}")
+//    private val topic: String,
 ) {
 
     fun sendMessage(bookInstance: BookInstance) {
@@ -14,6 +17,6 @@ class BookReservationProducer(
     }
 
     private companion object {
-        const val BOOK_RESERVATION_TOPIC = "book-reservation"
+        const val BOOK_RESERVATION_TOPIC = "core-catalog-reservations"
     }
 }
